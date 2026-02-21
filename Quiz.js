3b80c373b7912731b68303008e2c5cc6badb17ee
agudelodev <agudelojeronimo9@gmail.com>
2026-02-21 17:49:47 -0500

    CorreccionCodigo

---
+++ Quiz.js
@@ -0,0 +1,60 @@
+// PREGUNTA 1
+let mensaje1 = "¿Cuál es la capital de Francia?\nA) Londres\nB) París\nC) Berlín";
+let respuesta1 = prompt(mensaje1);
+let correcta1 = "B";
+
+if (respuesta1 === correcta1) {
+    alert("¡Correcto! La respuesta es B) París");
+} else {
+    alert("Incorrecto. La respuesta correcta es B) París");
+}
+
+// PREGUNTA 2
+let mensaje2 = "¿Cuántos continentes hay?\nA) 5\nB) 6\nC) 7";
+let respuesta2 = prompt(mensaje2);
+let correcta2 = "C";
+
+if (respuesta2 === correcta2) {
+    alert("¡Correcto! La respuesta es C) 7");
+} else {
+    alert("Incorrecto. La respuesta correcta es C) 7");
+}
+
+// PREGUNTA 3
+let mensaje3 = "¿Cuál es el planeta más grande del sistema solar?\nA) Júpiter\nB) Saturno\nC) Neptuno";
+let respuesta3 = prompt(mensaje3);
+let correcta3 = "A";
+
+if (respuesta3 === correcta3) {
+    alert("¡Correcto! La respuesta es A) Júpiter");
+} else {
+    alert("Incorrecto. La respuesta correcta es A) Júpiter");
+}
+
+// PREGUNTA 4
+let mensaje4 = "¿En qué año cayó el muro de Berlín?\nA) 1987\nB) 1989\nC) 1991";
+let respuesta4 = prompt(mensaje4);
+let correcta4 = "B";
+
+if (respuesta4 === correcta4) {
+    alert("¡Correcto! La respuesta es B) 1989");
+} else {
+    alert("Incorrecto. La respuesta correcta es B) 1989");
+}
+
+// PREGUNTA 5
+let mensaje5 = "¿Cuál es el océano más grande?\nA) Océano Atlántico\nB) Océano Ártico\nC) Océano Pacífico";
+let respuesta5 = prompt(mensaje5);
+let correcta5 = "C";
+
+if (respuesta5 === correcta5) {
+    alert("¡Correcto! La respuesta es C) Océano Pacífico");
+} else {
+    alert("Incorrecto. La respuesta correcta es C) Océano Pacífico");
+}
+
+if (respuesta5 === ) {
+    console.log("hoala")
+} else {
+    
+}
